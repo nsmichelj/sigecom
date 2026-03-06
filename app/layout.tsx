3123d7eaab7c789e3676cdc7b6d54d966407ac9d
nsmichelj <nunezsalmeronm@gmail.com>
2026-03-05 21:57:43 -0400

    Update RootLayout language to Spanish and enhance body styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}
       >
         {children}
       </body>
