d06cb6d796580576ac8a5b2f1009bfce21155d75
nsmichelj <nunezsalmeronm@gmail.com>
2026-04-01 14:47:28 -0400

    feat(pages): Implement query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { QueryProvider } from "@/components/providers/query-provider";
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}
       >
-        <Toaster />
-        {children}
+        <QueryProvider>
+          <Toaster richColors position="top-center" />
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
