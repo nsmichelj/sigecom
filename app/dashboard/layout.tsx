207ef4ba2eda8728a559bef881c5c7b07f2bf1ea
nsmichelj <nunezsalmeronm@gmail.com>
2026-04-01 14:46:49 -0400

    feat(pages): Add dashboard initial page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,35 @@
+import { Container } from "@/components/container";
+import { AppSidebar } from "@/components/dashboard/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Panel de Administración",
+  description: "Panel de Administración de SIGECOM",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 bg-background sticky top-0 border-b border-border z-50">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4">
+          <Container className="py-12">{children}</Container>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { DashboardHeader, DashboardTitle } from "@/components/dashboard/panel";
+
+export default function Dashboard() {
+  return (
+    <DashboardHeader>
+      <DashboardTitle>Panel Principal</DashboardTitle>
+    </DashboardHeader>
+  );
+}
